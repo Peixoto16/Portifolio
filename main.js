769cd3ed953d9b7e04e211711ae72fbe3b03b8eb
Israel Peixoto <israel1peixoto@gmail.com>
2023-10-07 02:27:34 -0300

    Menu Responsivo com JS

--- main.js
+++ main.js
@@ -49,3 +49,16 @@ e.preventDefault();
     alert("Dados Enviados");
 });
 
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
